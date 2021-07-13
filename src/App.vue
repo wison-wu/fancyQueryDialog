5ec71092f9035a75c8a02248d5cdf08b83179f22
wison <wurong715@163.com>
2021-07-13 14:28:59 +0800

    增加组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fancy-query-dialog v-model="value" :title="title" :showIndex="true"  :multi="false" :colConf="colConf" :action="action"/>
+    显示当前选择的值:{{value}}
+    <el-input v-model="title"></el-input>
+    字段配置<el-input v-model="colConf" type="textarea"></el-input>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import fancyQueryDialog from './components/fancyDialogList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    fancyQueryDialog
+  },
+  data() {
+      return {
+        value:'',
+        title:'test',
+        colConf:'[{"label":"姓名","property":"name","width":"250"},{"label":"日期","property":"date","width":"150"},{"label":"地址","property":"address","width":""}]',
+        action:'https://www.fastmock.site/mock/51715c0157535b99010bde55f2df33c8/formDesigner/api/dialogListData'
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+
+Vue.prototype.$axios = axios
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
